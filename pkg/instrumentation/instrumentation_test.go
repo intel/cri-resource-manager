489cad43e82388fbe2f6e55e2927c116480a5645
Krisztian Litkey <krisztian.litkey@intel.com>
2020-03-27 16:00:19 +0200

    instrumentation: add unit tests.

---
+++ pkg/instrumentation/instrumentation_test.go
@@ -0,0 +1,94 @@
+// Copyright 2019-2020 Intel Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package instrumentation
+
+import (
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestSamplingIdempotency(t *testing.T) {
+	tcases := []Sampling{
+		Disabled,
+		Testing,
+		Production,
+		0.2, 0.25, 0.5, 0.75, 0.8,
+	}
+	for _, tc := range tcases {
+		var chk Sampling
+		if err := chk.Parse(tc.String()); err != nil {
+			t.Errorf("failed to parse Sampling.String() %q: %v", tc, err)
+		}
+		if chk != tc {
+			t.Errorf("expected sampling value for %q: %v, got: %v", tc, tc, chk)
+		}
+	}
+}
+
+func TestPrometheusConfiguration(t *testing.T) {
+	flag.Set("logger-debug", "all")
+
+	s := newService()
+	s.Start()
+
+	checkPrometheus(t, opt.HTTPEndpoint, !opt.PrometheusExport)
+
+	opt.PrometheusExport = !opt.PrometheusExport
+	s.reconfigure()
+	checkPrometheus(t, opt.HTTPEndpoint, !opt.PrometheusExport)
+
+	opt.PrometheusExport = !opt.PrometheusExport
+	s.reconfigure()
+	checkPrometheus(t, opt.HTTPEndpoint, !opt.PrometheusExport)
+
+	opt.PrometheusExport = !opt.PrometheusExport
+	s.reconfigure()
+	checkPrometheus(t, opt.HTTPEndpoint, !opt.PrometheusExport)
+
+	s.http.Shutdown(true)
+	s.Stop()
+}
+
+func checkPrometheus(t *testing.T, server string, shouldFail bool) {
+	rpl, err := http.Get("http://" + server + "/metrics")
+
+	switch shouldFail {
+	case false:
+		if err != nil {
+			t.Errorf("Prometheus HTTP GET failed: %v", err)
+			return
+		}
+
+		if rpl.StatusCode != 200 {
+			t.Errorf("Prometheus HTTP GET failed: %s", rpl.Status)
+			return
+		}
+
+		_, err = ioutil.ReadAll(rpl.Body)
+		rpl.Body.Close()
+		if err != nil {
+			t.Errorf("failed to read Prometheus response: %v", err)
+		}
+		return
+
+	case true:
+		if err == nil && rpl.StatusCode == 200 {
+			t.Errorf("Prometheus HTTP GET should have failed, but it didn't.")
+			return
+		}
+	}
+}
