2f6d7f4c32474a570cca406e6d770817c512ebf8
Ismo Puustinen <ismo.puustinen@intel.com>
2020-05-20 18:58:01 +0300

    memtier: add initial code for dynamic page demotion.

--- pkg/cri/resource-manager/policy/builtin/memtier/flags.go
+++ pkg/cri/resource-manager/policy/builtin/memtier/flags.go
@@ -15,15 +15,21 @@
 package memtier
 
 import (
+	"fmt"
+	"time"
+
 	config "github.com/intel/cri-resource-manager/pkg/config"
 	"github.com/intel/cri-resource-manager/pkg/topology"
 )
 
+// Duration is an alias for time.Duration.
+type Duration time.Duration
+
 // Options captures our configurable policy parameters.
 type options struct {
-	// PinCPU controls CPU pinning in the topology-aware policy.
+	// PinCPU controls CPU pinning in the memtier policy.
 	PinCPU bool
-	// PinMemory controls memory pinning in the topology-aware policy.
+	// PinMemory controls memory pinning in the memtier policy.
 	PinMemory bool
 	// PreferIsolated controls whether isolated CPUs are preferred for isolated allocations.
 	PreferIsolated bool `json:"PreferIsolatedCPUs"`
@@ -31,6 +37,33 @@ type options struct {
 	PreferShared bool `json:"PreferSharedCPUs"`
 	// FakeHints are the set of fake TopologyHints to use for testing purposes.
 	FakeHints fakehints `json:",omitempty"`
+
+	DirtyBitScanPeriod Duration `json:"DirtyBitScanPeriod"`
+	PageMovePeriod     Duration `json:"PageMovePeriod"`
+	PageMoveCount      uint     `json:"PageMoveCount"`
+}
+
+// MarshalJSON converts Duration to JSON string.
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return []byte("\"" + time.Duration(d).String() + "\""), nil
+}
+
+// UnmarshalJSON converts JSON string to Duration.
+func (d *Duration) UnmarshalJSON(data []byte) error {
+	if len(data) < 2 {
+		return fmt.Errorf("invalid Duration data")
+	}
+	parsed, err := time.ParseDuration(string(data[1 : len(data)-1]))
+	if err != nil {
+		return err
+	}
+	*d = Duration(parsed)
+	return nil
+}
+
+// String returns the value of Duration as a string.
+func (d *Duration) String() string {
+	return time.Duration(*d).String()
 }
 
 // Our runtime configuration.
@@ -54,11 +87,14 @@ func (fh *fakehints) merge(hints fakehints) {
 // defaultOptions returns a new options instance, all initialized to defaults.
 func defaultOptions() interface{} {
 	return &options{
-		PinCPU:         true,
-		PinMemory:      true,
-		PreferIsolated: true,
-		PreferShared:   false,
-		FakeHints:      make(fakehints),
+		PinCPU:             true,
+		PinMemory:          true,
+		PreferIsolated:     true,
+		PreferShared:       false,
+		FakeHints:          make(fakehints),
+		DirtyBitScanPeriod: 0,
+		PageMovePeriod:     0,
+		PageMoveCount:      0,
 	}
 }
 
