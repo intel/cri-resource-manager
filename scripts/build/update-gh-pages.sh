ca0d1982f92fe550bfb0d5a7af7a352916657c97
Markus Lehtonen <markus.lehtonen@intel.com>
2020-10-09 15:38:42 +0300

    scripts: add script for updating gh-pages branch
    The new script builds documentation from the currently checked-out
    commit and update the gh-pages branch. In more detail, it updates a
    subfolder (in the gh-pages branch) corresponding the major version  of
    the software. The version used here is parsed from the GITHUB_REF
    environment variable and defaults to 'devel' if it is not defined. Also,
    for master branch use subfolder name 'devel' instead of using the branch
    name (which would be the case for other non-release branches). The
    "version subfolder" can also be given directly from the command line,
    e.g. for testing purposes.
    
    update-gh-pages.sh: map 'master' to 'devel'

---
+++ scripts/build/update-gh-pages.sh
@@ -0,0 +1,137 @@
+#!/bin/bash -e
+set -o pipefail
+
+script=`basename $0`
+
+usage () {
+cat << EOF
+Usage: $script [-h] [-a] [BUILD_SUBDIR]
+
+Options:
+  -h         show this help and exit
+  -a         amend (with --reset-author) instead of creating a new commit
+EOF
+}
+
+#
+# Argument parsing
+#
+while [ "${1#-}" != "$1" -a -n "$1" ]; do
+    case "$1" in
+        -a|--amend)
+            amend="--amend --reset-author"
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+build_subdir="$1"
+
+# Check that no extra args were provided
+if [ $# -gt 1 ]; then
+    echo "ERROR: unknown arguments: $@"
+    usage
+    exit 1
+fi
+
+#
+# Build the documentation
+#
+build_dir="_build-ghpages"
+echo "Creating new Git worktree at $build_dir"
+git worktree add "$build_dir" gh-pages
+
+# Drop worktree on exit
+trap "echo 'Removing Git worktree $build_dir'; git worktree remove --force '$build_dir'" EXIT
+
+# Parse subdir name from GITHUB_REF
+if [ -z "$build_subdir" ]; then
+    case "$GITHUB_REF" in
+        refs/tags/*)
+            _base_ref=${GITHUB_REF#refs/tags/}
+            ;;
+        refs/heads/*)
+            _base_ref=${GITHUB_REF#refs/heads/}
+            ;;
+        *) _base_ref=
+    esac
+    echo "Parsed baseref: '$_base_ref'"
+
+    case "$GITHUB_REF" in
+        refs/tags/v*)
+            _version=${GITHUB_REF#refs/tags/v}
+            ;;
+        refs/heads/release-*)
+            _version=${GITHUB_REF#refs/heads/release-}
+            ;;
+        *) _version=
+    esac
+    echo "Detected version: '$_version'"
+
+    _version=`echo -n $_version | sed -nE s'!^([0-9]+\.[0-9]+).*$!\1!p'`
+
+    # Use version as the subdir
+    build_subdir=${_version:+v$_version}
+    # Fallback to base-ref i.e. name of the branch or tag
+    if [ -z "$build_subdir" ]; then
+        # For master branch we use the name 'devel'
+        [ "$_base_ref" = "master" ] && build_subdir=devel || build_subdir=$_base_ref
+    fi
+fi
+
+# Default to 'devel' if no subdir was given and we couldn't parse
+# it
+build_subdir=${build_subdir:-devel}
+echo "Updating site version subdir: '$build_subdir'"
+export SITE_BUILDDIR="$build_dir/$build_subdir"
+
+# Detect existing versions from the gh-pages branch
+# 'stable' is a symlink pointing to the latest version
+cd "$build_dir"
+export VERSIONS="$build_subdir ${_version:+stable} `ls -d */  | tr -d /`"
+cd -
+
+make html
+
+#
+# Update gh-pages branch
+#
+commit_hash=`git describe --dirty --always`
+
+# Switch to work in the gh-pages worktree
+cd "$build_dir"
+
+# Add "const" files we need in root dir
+touch .nojekyll
+
+_stable=`ls -d1 v*/ || : | sort -n | tail -n1`
+[ -n "$_stable" ] && ln -sfT "$_stable" stable
+
+cat > index.html << EOF
+<meta http-equiv="refresh" content="0; URL='devel'" />
+EOF
+
+if [ -z "`git status --short`" ]; then
+    echo "No new content, gh-pages branch already up-to-date"
+    exit 0
+fi
+
+# Create a new commit
+commit_msg=`echo -e "Update documentation for $build_subdir\n\nAuto-generated from $commit_hash by '$script'"`
+
+echo "Committing changes..."
+# Exclude doctrees dir
+git add -- ":!$build_subdir/.doctrees"
+git commit $amend -m "$commit_msg"
+
+cd -
+
+echo "gh-pages branch successfully updated"
