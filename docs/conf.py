4bc8243d88d80414009d5833dc8c472d8103a508
Markus Lehtonen <markus.lehtonen@intel.com>
2020-10-13 15:35:51 +0300

    docs: archive documentation for tagged releases
    Use a separate 'releases' subdir for the documentation archive. The root
    of this subdir is effectively a separate site built with sphinx. Site
    configuration lies under 'docs/releases' and it is only being (re-)built
    on the master branch. This is to ensure that master branch is in control
    of the releases page and no older release branches (or tags) can mess it
    up. The actual list of releases shown on the page is dynaically
    generated, in a similar fashion to the version menu. The releases/
    directory is supposed to have releases.js which lists all archived
    releases. Unlike the actual html content, this "release metadata" will
    be updated on every execution of update-gh-pages.sh.

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '_work', 'generate', 'README.md', 'RELEASE.md']
+exclude_patterns = ['_build', '_work', 'generate', 'README.md', 'RELEASE.md', 'docs/releases']
 
 
 # -- Options for HTML output -------------------------------------------------
