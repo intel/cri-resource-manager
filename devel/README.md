eb8a053df82df9bf028a0df4d347f1c9783d153c
Github <no-reply@github.com>
2020-10-15 08:06:35 +0000

    Update documentation for devel
    Auto-generated from v0.4.0-94-g1c73423 by 'update-gh-pages.sh'

--- devel/README.md
+++
@@ -1,8 +0,0 @@
-# CRI Resource Manager for Kubernetes
-
-Welcome!
-
-### See our [Documentation][documentation] site for detailed documentation.
-
-[documentation]: https://intel.github.io/cri-resource-manager
-
--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0-91-g1e8c01a',
+    VERSION: '0.4.0-94-g1c73423',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
