99d71d210131e8d3951aac890511c3a1c94a5879
Antti Kervinen <antti.kervinen@intel.com>
2020-11-05 15:24:34 +0200

    e2e: add static-pools test for CMK_CPUS_{SHARED,INFRA}

--- test/e2e/policies.test-suite/static-pools/n4c16/test04-cmk-isolate-noaffinity/code.var.sh
+++ test/e2e/policies.test-suite/static-pools/n4c16/test04-cmk-isolate-noaffinity/code.var.sh
@@ -5,6 +5,7 @@
 # shellcheck disable=SC2148
 cri_resmgr_cfg="$TEST_DIR/../cri-resmgr-static-pools.cfg" static-pools-relaunch-cri-resmgr
 export STP_POOL="" STP_SOCKET_ID="" CMDSPLIT="command_all"
+export ECHO_VARS='CMK_CPUS_ASSIGNED="$CMK_CPUS_ASSIGNED" CMK_CPUS_SHARED="$CMK_CPUS_SHARED" CMK_CPUS_INFRA="$CMK_CPUS_INFRA"'
 
 export CMK_ISOLATE=", '--conf-dir=/etc/cmk.conf', '--pool=exclusive', '--socket-id=0', '--no-affinity'"
 out ""
@@ -13,8 +14,14 @@ CPU=1000m EXCLCORES=1 create cmk-isolate
 report allowed
 verify 'len(cores["pod0c0"]) == 8'
 cpus_assigned="$(kubectl logs pod0 | tail -n 1 | awk '{print $2}')"
+cpus_shared="$(kubectl logs pod0 | tail -n 1 | awk '{print $3}')"
+cpus_infra="$(kubectl logs pod0 | tail -n 1 | awk '{print $4}')"
 [ "$cpus_assigned" == "CMK_CPUS_ASSIGNED=0,1" ] ||
     error "expected CMK_CPUS_ASSIGNED=0,1, got $cpus_assigned"
+[ "$cpus_shared" == "CMK_CPUS_SHARED=4-6,7" ] ||
+    error "expected CMK_CPUS_SHARED=4-6,7, got $cpus_shared"
+[ "$cpus_infra" == "CMK_CPUS_INFRA=10-15" ] ||
+    error "expected CMK_CPUS_INFRA=10-15, got $cpus_infra"
 
 export CMK_ISOLATE=", '--conf-dir=/etc/cmk.conf', '--pool=exclusive', '--socket-id=1', '--no-affinity'"
 out ""
