5b588a61c1569abd488acde6a15e3922d399699a
Github <no-reply@github.com>
2020-11-27 15:55:35 +0000

    Update documentation for v0.4
    Auto-generated from v0.4.0-181-g8a1c220 by 'update-gh-pages.sh'

--- v0.4/_static/documentation_options.js
+++ v0.4/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0-100-g1758d3b',
+    VERSION: '0.4.0-181-g8a1c220',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
