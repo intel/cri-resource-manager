df74f9712115fda33920bfb6fd0f9156d77f6e09
Github <no-reply@github.com>
2021-10-06 14:23:58 +0000

    Update documentation for v0.6
    Auto-generated from v0.6.0 by 'update-gh-pages.sh'

--- versions.js
+++ versions.js
@@ -4,5 +4,6 @@ function getVersionsMenuItems() {
     { name: 'stable', url: '/cri-resource-manager/stable' },
     { name: 'v0.4', url: '/cri-resource-manager/v0.4' },
     { name: 'v0.5', url: '/cri-resource-manager/v0.5' },
+    { name: 'v0.6', url: '/cri-resource-manager/v0.6' },
   ];
 }
