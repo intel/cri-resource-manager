044565f550dd5789d379e791bc9d1bdc3f123389
Github <no-reply@github.com>
2021-10-11 11:09:18 +0000

    Update documentation for v0.6
    Auto-generated from v0.6.0 by 'update-gh-pages.sh'

--- releases/releases.js
+++ releases/releases.js
@@ -2,5 +2,6 @@ function getReleaseListItems() {
   return [
     { name: 'v0.4.1', url: 'v0.4.1' },
     { name: 'v0.5.0', url: 'v0.5.0' },
+    { name: 'v0.6.0', url: 'v0.6.0' },
   ];
 }
---
+++ releases/v0.6.0/_sources/docs/developers-guide/testing.rst.txt
@@ -0,0 +1,9 @@
+Testing
+#######
+
+.. toctree::
+   :maxdepth: 1
+
+   unit-test.md
+   cri-test.md
+   e2e-test.md
