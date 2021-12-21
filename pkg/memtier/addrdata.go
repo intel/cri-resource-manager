03f52f98c78e272f35983569335e75d4bb603319
Antti Kervinen <antti.kervinen@intel.com>
2021-12-21 17:25:03 +0200

    memtier: add a data structure for storing generic page information
    - The addrdata data structure implements a low-memory-footprint
      storage for keeping track on which NUMA node a page is associated
      with. (Where a page is or where it is expected to be...)

---
+++ pkg/memtier/addrdata.go
@@ -0,0 +1,151 @@
+// Copyright 2021 Intel Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memtier
+
+import (
+	"fmt"
+	"strings"
+)
+
+// AddrData is a data structure for storing arbitrary data on memory
+// addresses.
+type AddrData struct {
+	AddrRange
+	data interface{}
+}
+
+// AddrDatas slice is sorted by non-overlapping address ranges.
+type AddrDatas struct {
+	ads []*AddrData
+}
+
+func NewAddrData(addr, length uint64, data interface{}) *AddrData {
+	return &AddrData{
+		AddrRange: AddrRange{
+			addr:   addr,
+			length: length,
+		},
+		data: data,
+	}
+}
+
+func NewAddrDatas() *AddrDatas {
+	return &AddrDatas{
+		ads: []*AddrData{},
+	}
+}
+
+// Data(addr) fetch data associated with an address.
+// data, ok := ads.Data(addr)
+// behaves similarly to
+// value, ok := map[key]
+func (a *AddrDatas) Data(addr uint64) (interface{}, bool) {
+	first, count := a.overlapping(&AddrRange{addr, 1})
+	if count > 0 && a.ads[first].addr <= addr {
+		return a.ads[first].data, true
+	}
+	return nil, false
+}
+
+func (a *AddrDatas) Dump() string {
+	sl := []string{}
+	for _, ad := range a.ads {
+		firstPage := ad.addr / constUPagesize
+		lastPage := firstPage + ad.length - 1
+		if lastPage != firstPage {
+			sl = append(sl, fmt.Sprintf("%d-%d:%v", firstPage, lastPage, ad.data))
+		} else {
+			sl = append(sl, fmt.Sprintf("%d:%v", firstPage, ad.data))
+		}
+	}
+	return "AddrDatas{" + strings.Join(sl, ",") + "}"
+}
+
+func (a *AddrDatas) Overwrite(ad *AddrData) {
+	first, count := a.overlapping(&ad.AddrRange)
+	last := first + count - 1
+	newLen := len(a.ads) - count + 1
+	if count > 0 {
+		if a.ads[first].addr < ad.addr {
+			newLen += 1
+		}
+		if a.ads[last].EndAddr() > ad.EndAddr() {
+			newLen += 1
+		}
+	}
+	newAds := make([]*AddrData, 0, newLen)
+	newAds = append(newAds, a.ads[0:first]...)
+	if count > 0 {
+		if a.ads[first].addr < ad.addr {
+			// Case:
+			// |---------ads[first]--...
+			//            |---ad-----...
+			// =>
+			// |---TBD----|---ad-----...
+			var newFirst *AddrData
+			if count == 1 && a.ads[first].EndAddr() > ad.EndAddr() {
+				// Case: first == last, cannot reuse ads[first] twice
+				// |---------ads[first]-----------|
+				//            |---ad---|
+				// =>
+				// |-newFirst-|---ad---|ads[first]|
+				newFirst = NewAddrData(a.ads[first].addr,
+					a.ads[first].length,
+					a.ads[first].data)
+			} else {
+				// Case: reuse ads[first]
+				// |---------ads[first]--...
+				//            |---ad-----...
+				// =>
+				// |ads[first]|---ad-----...
+				newFirst = a.ads[first]
+			}
+			newFirst.length = (ad.addr - newFirst.addr) / constUPagesize
+			newAds = append(newAds, newFirst)
+		}
+	}
+	newAds = append(newAds, ad)
+	if count > 0 {
+		if a.ads[last].EndAddr() > ad.EndAddr() {
+			a.ads[last].length = (a.ads[last].EndAddr() - ad.EndAddr()) / constUPagesize
+			a.ads[last].addr = ad.EndAddr()
+			newAds = append(newAds, a.ads[last])
+		}
+	}
+	if last+1 < len(a.ads) {
+		newAds = append(newAds, a.ads[last+1:]...)
+	}
+	a.ads = newAds
+}
+
+func (a *AddrDatas) overlapping(ar0 *AddrRange) (int, int) {
+	// Optimize: bisect would be faster way to find the first overlapping hr
+	first := 0
+	count := 0
+	for _, ad := range a.ads {
+		if ad.EndAddr() > ar0.addr {
+			break
+		}
+		first++
+	}
+	ar0EndAddr := ar0.EndAddr()
+	for _, ad := range a.ads[first:] {
+		if ar0EndAddr <= ad.addr {
+			break
+		}
+		count += 1
+	}
+	return first, count
+}
---
+++ pkg/memtier/addrdata_test.go
@@ -0,0 +1,203 @@
+// Copyright 2021 Intel Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memtier
+
+import (
+	"testing"
+)
+
+func TestOverwrite(t *testing.T) {
+	ads := NewAddrDatas()
+	PS := constUPagesize
+	expectDataOk := func(addr uint64, expectedData int, expectedOk bool) {
+		data, ok := ads.Data(addr)
+		if ok != expectedOk {
+			t.Errorf("Data(%x (page %d)): expected ok == %v, got %v", addr, addr/constUPagesize, expectedOk, ok)
+			return
+		}
+		if ok {
+			if data != expectedData {
+				t.Errorf("Data(%x): expected data == %v, got %v",
+					addr, expectedData, data)
+				return
+			}
+		}
+
+	}
+	expectLen := func(expectedLen int) {
+		if len(ads.ads) != expectedLen {
+			t.Errorf("expected len == %v, got %v", expectedLen, len(ads.ads))
+		}
+	}
+	// No data in the structure, must not crash.
+	expectLen(0)
+	expectDataOk(0, 0, false)
+	expectDataOk(1000*PS, 0, false)
+
+	// Single data block, 10 pages starting from the 10th page.
+	ads.Overwrite(NewAddrData(10*PS, 10, 1))
+	expectLen(1)
+	expectDataOk(0, 0, false)
+	expectDataOk(9*PS, 0, false)
+	expectDataOk(10*PS-1, 0, false)
+	expectDataOk(10*PS, 1, true)
+	expectDataOk(10*PS+1, 1, true)
+	expectDataOk(11*PS, 1, true)
+	expectDataOk(19*PS, 1, true)
+	expectDataOk(20*PS-1, 1, true)
+	expectDataOk(20*PS, 0, false)
+	expectDataOk(20*PS+1, 0, false)
+	expectDataOk(21*PS, 0, false)
+
+	// Add non-overlapping data, new first element
+	ads.Overwrite(NewAddrData(5*PS, 2, 2))
+	expectLen(2)
+	expectDataOk(4*PS, 0, false)
+	expectDataOk(5*PS, 2, true)
+	expectDataOk(6*PS, 2, true)
+	expectDataOk(7*PS, 0, false)
+	expectDataOk(8*PS, 0, false)
+	expectDataOk(9*PS, 0, false)
+	expectDataOk(10*PS, 1, true)
+
+	// Add non-overlapping data, new last element
+	ads.Overwrite(NewAddrData(25*PS, 4, 3))
+	expectLen(3)
+	expectDataOk(19*PS, 1, true)
+	expectDataOk(20*PS, 0, false)
+	expectDataOk(21*PS, 0, false)
+	expectDataOk(22*PS, 0, false)
+	expectDataOk(23*PS, 0, false)
+	expectDataOk(24*PS, 0, false)
+	expectDataOk(25*PS, 3, true)
+	expectDataOk(26*PS, 3, true)
+	expectDataOk(27*PS, 3, true)
+	expectDataOk(28*PS, 3, true)
+	expectDataOk(29*PS, 0, false)
+
+	// Overwrite data in the middle of existing element
+	ads.Overwrite(NewAddrData(26*PS, 1, 4))
+	expectLen(5)
+	expectDataOk(24*PS, 0, false)
+	expectDataOk(25*PS, 3, true)
+	expectDataOk(26*PS, 4, true)
+	expectDataOk(27*PS, 3, true)
+	expectDataOk(28*PS, 3, true)
+	expectDataOk(29*PS, 0, false)
+
+	// Overwrite completely over an old element
+	ads.Overwrite(NewAddrData(3*PS, 5, 5))
+	expectLen(5)
+	expectDataOk(2*PS, 0, false)
+	expectDataOk(3*PS, 5, true)
+	expectDataOk(4*PS, 5, true)
+	expectDataOk(5*PS, 5, true)
+	expectDataOk(6*PS, 5, true)
+	expectDataOk(7*PS, 5, true)
+	expectDataOk(8*PS, 0, false)
+	expectDataOk(9*PS, 0, false)
+	expectDataOk(10*PS, 1, true)
+
+	// Fill a hole with exact match
+	ads.Overwrite(NewAddrData(8*PS, 2, 6))
+	expectLen(6)
+	expectDataOk(7*PS, 5, true)
+	expectDataOk(8*PS, 6, true)
+	expectDataOk(9*PS, 6, true)
+	expectDataOk(10*PS, 1, true)
+
+	// Partial overwrite the beginning of existing data
+	ads.Overwrite(NewAddrData(1*PS, 3, 7))
+	expectLen(7)
+	expectDataOk(0*PS, 0, false)
+	expectDataOk(1*PS, 7, true)
+	expectDataOk(2*PS, 7, true)
+	expectDataOk(3*PS, 7, true)
+	expectDataOk(4*PS, 5, true)
+
+	// Partial overwrite the end of existing data
+	ads.Overwrite(NewAddrData(28*PS, 2, 8))
+	expectLen(8)
+	expectDataOk(24*PS, 0, false)
+	expectDataOk(25*PS, 3, true)
+	expectDataOk(26*PS, 4, true)
+	expectDataOk(27*PS, 3, true)
+	expectDataOk(28*PS, 8, true)
+	expectDataOk(29*PS, 8, true)
+	expectDataOk(30*PS, 0, false)
+
+	// Exact overwrite existing data
+	ads.Overwrite(NewAddrData(8*PS, 2, 9))
+	expectLen(8)
+	expectDataOk(7*PS, 5, true)
+	expectDataOk(8*PS, 9, true)
+	expectDataOk(9*PS, 9, true)
+	expectDataOk(10*PS, 1, true)
+
+	// Exact overwrite many existing datas
+	ads.Overwrite(NewAddrData(25*PS, 3, 10))
+	expectLen(6) // remove 25:3, 26:4, 27:3, add 25-27:10
+	expectDataOk(24*PS, 0, false)
+	expectDataOk(25*PS, 10, true)
+	expectDataOk(26*PS, 10, true)
+	expectDataOk(27*PS, 10, true)
+	expectDataOk(28*PS, 8, true)
+	expectDataOk(29*PS, 8, true)
+	expectDataOk(30*PS, 0, false)
+
+	// Overlapping overwrite many existing datas
+	ads.Overwrite(NewAddrData(24*PS, 5, 11))
+	expectLen(6) // remove 25-27:10, add 24-28:11
+	expectDataOk(23*PS, 0, false)
+	expectDataOk(24*PS, 11, true)
+	expectDataOk(25*PS, 11, true)
+	expectDataOk(26*PS, 11, true)
+	expectDataOk(27*PS, 11, true)
+	expectDataOk(28*PS, 11, true)
+	expectDataOk(29*PS, 8, true)
+	expectDataOk(30*PS, 0, false)
+
+	// Overwrite from the middle to the middle
+	ads.Overwrite(NewAddrData(18*PS, 7, 12))
+	expectLen(7)
+	expectDataOk(17*PS, 1, true)
+	expectDataOk(18*PS, 12, true)
+	expectDataOk(19*PS, 12, true)
+	expectDataOk(20*PS, 12, true)
+	expectDataOk(21*PS, 12, true)
+	expectDataOk(22*PS, 12, true)
+	expectDataOk(23*PS, 12, true)
+	expectDataOk(24*PS, 12, true)
+	expectDataOk(25*PS, 11, true)
+
+	// Overwrite everything but the first and the last datas
+	ads.Overwrite(NewAddrData(2*PS, 27, 13))
+	expectLen(3)
+	expectDataOk(0*PS, 0, false)
+	expectDataOk(1*PS, 7, true)
+	expectDataOk(2*PS, 13, true)
+	expectDataOk(27*PS, 13, true)
+	expectDataOk(28*PS, 13, true)
+	expectDataOk(29*PS, 8, true)
+
+	// Overwrite everything
+	ads.Overwrite(NewAddrData(1*PS, 29, 14))
+	expectLen(1)
+	expectDataOk(0*PS, 0, false)
+	expectDataOk(1*PS, 14, true)
+	expectDataOk(28*PS, 14, true)
+	expectDataOk(29*PS, 14, true)
+	expectDataOk(30*PS, 0, false)
+}
