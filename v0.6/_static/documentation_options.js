d9c5f57101cef31d53172f614015471b9898bcd3
Github <no-reply@github.com>
2022-02-16 13:06:51 +0000

    Update documentation for v0.6
    Auto-generated from v0.6.0-8-gb4977b5e by 'update-gh-pages.sh'

--- v0.6/_static/documentation_options.js
+++ v0.6/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.0-6-g798bcba0',
+    VERSION: '0.6.0-8-gb4977b5e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
