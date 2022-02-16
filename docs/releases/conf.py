734e0af8fc4de8032bfcef21bce326c206267feb
Markus Lehtonen <markus.lehtonen@intel.com>
2022-02-16 10:12:44 +0200

    docs: use myst markdown parser for docs/releases too
    Supplements 09cc1ee31db37f2f634f8a58d3f7b46704764b0f.
    
    (cherry picked from commit 009a83eb65cf3e9cf39bc4766fb114e782e60bd2)

--- docs/releases/conf.py
+++ docs/releases/conf.py
@@ -34,7 +34,7 @@ if os.getenv('VERSIONS_MENU'):
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'recommonmark',
+        'myst_parser',
         'sphinx_markdown_tables'
         ]
 source_suffix = {
