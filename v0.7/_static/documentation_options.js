2c782abd0f3655d55f743aaf470a040e1dc337ce
Github <no-reply@github.com>
2022-08-31 06:51:44 +0000

    Update documentation for v0.7
    Auto-generated from v0.7.1-3-g7471f96d by 'update-gh-pages.sh'

--- v0.7/_static/documentation_options.js
+++ v0.7/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.1',
+    VERSION: '0.7.1-3-g7471f96d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
