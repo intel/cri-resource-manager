30300948a9f1dd8d177db33a46582bcb4a5f09ac
Antti Kervinen <antti.kervinen@intel.com>
2023-08-14 16:14:55 +0300

    e2e: restore vm as last step in the static-pools test suite

---
+++ test/e2e/policies.test-suite/static-pools/n4c16/test99-cleanup/code.var.sh
@@ -0,0 +1,5 @@
+# This test cleans up static-pools test suite configurations from vm.
+# Other policy tests can be run after this test on the same vm without
+# recreating the vm from scratch.
+
+static-pools-cleanup
