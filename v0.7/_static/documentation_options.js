b8d9d413a33656f09f2dd758f7ec7749c36d1a43
Markus Lehtonen <markus.lehtonen@intel.com>
2025-01-17 12:46:45 +0000

    v0.7: add a banner indicating EOL
    Auto-generated from v0.7.2-4-g0bb3518e-dirty by 'update-gh-pages.sh'

--- v0.7/_static/documentation_options.js
+++ v0.7/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.2-4-g0bb3518e',
+    VERSION: '0.7.2-5-g767f5bfd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
